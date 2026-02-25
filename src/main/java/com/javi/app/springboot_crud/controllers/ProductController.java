30c91aa8e0e9fcd401a56b74f230ae8646e6aec4
Javi2398 <javi2398@gmail.com>
2026-02-25 12:00:26 +0100

    Añadimos validacion

--- src/main/java/com/javi/app/springboot_crud/controllers/ProductController.java
+++ src/main/java/com/javi/app/springboot_crud/controllers/ProductController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.javi.app.springboot_crud.entities.Product;
 import com.javi.app.springboot_crud.services.ProductService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/products")
@@ -40,12 +43,12 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> create(@RequestBody Product product) {
+    public ResponseEntity<Product> create(@Valid @RequestBody Product product) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.save(product));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product) {
+    public ResponseEntity<Product> update(@PathVariable Long id, @Valid @RequestBody Product product) {
         Optional<Product> productOptional = service.update(id, product);
         if (productOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.OK).body(productOptional.orElseThrow());
