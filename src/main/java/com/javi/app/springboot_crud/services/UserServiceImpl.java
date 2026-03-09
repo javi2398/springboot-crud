dca479f4e5f03adf267a1e87fef3a79e488cd96e
javi2398 <javi2398@gmail.com>
2026-03-09 17:29:31 +0100

    metodos userservice

--- src/main/java/com/javi/app/springboot_crud/services/UserServiceImpl.java
+++ src/main/java/com/javi/app/springboot_crud/services/UserServiceImpl.java
@@ -1,12 +1,15 @@
 package com.javi.app.springboot_crud.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.javi.app.springboot_crud.entities.Role;
 import com.javi.app.springboot_crud.entities.User;
 import com.javi.app.springboot_crud.repositories.RoleRepository;
 import com.javi.app.springboot_crud.repositories.UserRepository;
@@ -32,6 +35,20 @@ public class UserServiceImpl implements UserService{
     @Override
     @Transactional
     public User save(User user) {
+
+        Optional<Role> optionalRoleUser = roleRepository.findByName("ROLE_USER");
+        List<Role> roles = new ArrayList<>();
+
+        optionalRoleUser.ifPresent(roles::add);
+
+        if(user.isAdmin()) {
+            Optional<Role> optionalRoleAdmin = roleRepository.findByName("ROLE_ADMIN");
+            optionalRoleAdmin.ifPresent(roles::add);
+        }
+
+        user.setRoles(roles);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
         return repository.save(user);
     };
 
